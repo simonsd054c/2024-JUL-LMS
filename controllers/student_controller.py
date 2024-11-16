b1233d6787226a419bb68be7c2e1be4bd585a2e8
Simon Dahal <simon.dahal@coderacademy.edu.au>
2024-11-16 11:59:28 +1100

    Implement create operation on student entity

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
 
 from init import db
 from models.student import Student, students_schema, student_schema
@@ -31,3 +33,31 @@ def get_student(student_id):
         return data
     else:
         return {"message": f"Student with id {student_id} does not exist"}, 404
+
+
+# Create - /students - POST
+@students_bp.route("/", methods=["POST"])
+def create_student():
+    try:
+        # get information from the request body
+        body_data = request.get_json()
+        # create student instance
+        new_student = Student(
+            name=body_data.get("name"),
+            email=body_data.get("email"),
+            address=body_data.get("address")
+        )
+        # add to the session
+        db.session.add(new_student)
+        # commit
+        db.session.commit()
+        # return a response
+        return student_schema.dump(new_student), 201
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            # not null violation
+            return {"message": f"The field '{err.orig.diag.column_name}' is required"}, 409
+        
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            # unique constraint violation
+            return {"message": "Email address already in use"}, 409
