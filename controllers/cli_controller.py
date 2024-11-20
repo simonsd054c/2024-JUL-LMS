3bfd2b6184958c1b03f653e3373778d65811aa11
Simon Dahal <simon.dahal@coderacademy.edu.au>
2024-11-20 20:02:02 +1100

    Seed courses table

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from init import db
 from models.student import Student
 from models.teacher import Teacher
+from models.course import Course
 
 db_commands = Blueprint("db", __name__)
 
@@ -51,6 +52,33 @@ def seed_tables():
     ]
 
     db.session.add_all(teachers)
+
+    db.session.commit()
+
+    courses = [
+        Course(
+            name="Course 1",
+            duration=1,
+            teacher_id=teachers[0].id
+        ),
+        Course(
+            name="Course 2",
+            duration=2,
+            teacher_id=teachers[0].id
+        ),
+        Course(
+            name="Course 3",
+            duration=2,
+            teacher_id=teachers[1].id
+        ),
+        Course(
+            name="Course 4",
+            duration=3,
+            teacher_id=teachers[0].id
+        )
+    ]
+
+    db.session.add_all(courses)
 
     db.session.commit()
 
