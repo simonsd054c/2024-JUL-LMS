0505601f95c8d21fbd820afb1c6eadd21d819cc7
Simon Dahal <simon.dahal@coderacademy.edu.au>
2024-11-23 12:29:37 +1100

    Create Enrolment model and schema

--- models/course.py
+++ models/course.py
@@ -13,6 +13,8 @@ class Course(db.Model):
 
     teacher = db.relationship("Teacher", back_populates="courses")
 
+    enrolments = db.relationship("Enrolment", back_populates="course", cascade="all, delete")
+
 # id: 1,
 # name: "Course 1",
 # duration: 1,
@@ -21,14 +23,29 @@ class Course(db.Model):
 #   id: 1,
 #   name: "Teacher 1",
 #   department: "Engineering"
+# },
+# enrolments: [
+# {
+#   id: 1,
+#   enrolment_date: "2022-10-20",
+#   student_id: 1,
+#   course_id: 2,
+# },
+# {
+#   id: 2,
+#   enrolment_date: "2024-08-20",
+#   student_id: 2,
+#   course_id: 1,
 # }
+# ]
 
 
 class CourseSchema(ma.Schema):
     ordered=True
     teacher = fields.Nested("TeacherSchema", only=["name", "department"])
+    enrolments = fields.List(fields.Nested("EnrolmentSchema", exclude=["course"]))
     class Meta:
-        fields = ("id", "name", "duration", "teacher_id", "teacher")
+        fields = ("id", "name", "duration", "teacher_id", "teacher", "enrolments")
 
 
 course_schema = CourseSchema()
---
+++ models/enrolment.py
@@ -0,0 +1,26 @@
+from marshmallow import fields
+
+from init import db, ma
+
+
+class Enrolment(db.Model):
+    __tablename__ = "enrolments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    enrolment_date = db.Column(db.Date)
+    student_id = db.Column(db.Integer, db.ForeignKey("students.id"), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.id"), nullable=False)
+
+    student = db.relationship("Student", back_populates="enrolments")
+    course = db.relationship("Course", back_populates="enrolments")
+
+
+class EnrolmentSchema(ma.Schema):
+    student = fields.Nested("StudentSchema", only=["name", "email"])
+    course = fields.Nested("CourseSchema", only=["name", "duration"])
+    class Meta:
+        fields = ("id", "enrolment_date", "student_id", "course_id", "student", "course")
+
+
+enrolment_schema = EnrolmentSchema()
+enrolments_schema = EnrolmentSchema(many=True)
